315a1148004fd55cadbd17abf804c55ba6662197
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-01-29 16:17:38 +0200

    footer+main styling

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ConfigService } from '../../config.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  enabled = false;
+  sticky = false;
+  private configService = inject(ConfigService);
 
+  async ngOnInit() {
+    try{
+      const config = await this.configService.loadConfig();
+      console.log('Config loaded:', config);
+      this.enabled = config.footer.enabled;
+      this.sticky = config.footer.sticky;
+    }catch (error) {
+      console.error('Error loading footer configuration:', error);
+    }
+  }
 }
