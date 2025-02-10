4e542a79956484c044cb55d9d2bce1dc807941e3
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-10 12:36:04 +0200

    service for dropdowns

--- src/app/dropdown.service.ts
+++ src/app/dropdown.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ApiService } from './api-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DropdownService {
+  private countriesSource = new BehaviorSubject<string[]>([]);
+  currentCountries = this.countriesSource.asObservable();
 
-  constructor() {
+
+  private citiesSource = new BehaviorSubject<string[]>([]);
+  cities$ = this.citiesSource.asObservable();
+
+  constructor(private apiService: ApiService) {
+  }
+
+ loadCountries() {
+    this.apiService.getCountries().subscribe(
+      (countries) => {
+        console.log("Countries loaded successfully:", countries);
+        this.countriesSource.next(countries);
+      },
+      (error) => {
+        console.error("Error loading countries:", error);
+        this.countriesSource.next([]); 
+      }
+    );
+  }
+
+  loadCities(countryCode: string) {
+    this.apiService.getCities(countryCode).subscribe((response: any) => {  
+      if (!Array.isArray(response)) {
+        console.error("Error: Unexpected API response structure:", response);
+        return;
+      }
+  
+      this.citiesSource.next(response); 
+    });
   }
 }
