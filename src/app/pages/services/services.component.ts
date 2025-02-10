a71d7f581873afad35d11d15101a73e668d5c447
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-10 12:42:48 +0200

    html countries

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -3,11 +3,12 @@ import { Component, inject, Input } from '@angular/core';
 import { ContentService } from '../../content.service';
 import { LanguageService } from '../../lang.service';
 import { DropdownService } from '../../dropdown.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './services.component.html',
   styleUrl: '../home/home.component.css'
 })
@@ -33,7 +34,14 @@ export class ServicesComponent {
     });
 
     this.dropdownService.currentCountries.subscribe(countries => this.countries = countries);
-
+    this.dropdownService.cities$.subscribe(cities => this.cities = cities);
     this.dropdownService.loadCountries();
+  }
+
+  onCountryChange(event: Event) {
+    const country = (event.target as HTMLSelectElement).value;
+    this.selectedCountry = country;
+    console.log("country selected: ", country);
+    this.dropdownService.loadCities(country);
   }
 }
